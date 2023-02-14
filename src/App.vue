fa0c52f7e4dd108746fb50be811c82eb7a2866a6
cobankar <lhuber.jona@gmail.com>
2023-02-15 00:11:02 +0100

    Change title and file names

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-  import Logo from './components/Logo.vue';
-  import Shortener from './components/Shortener.vue';
+import Logo from "./components/AppLogo.vue";
+import Shortener from "./components/LinkShortener.vue";
 </script>
 
 <template>
-    <header>
-      <Logo />
-    </header>
+  <header>
+    <Logo />
+  </header>
 
-    <main>
-      <Shortener />
-    </main>
+  <main>
+    <Shortener />
+  </main>
 </template>
 
 <style scoped>
-
 header {
   line-height: 1.5;
   margin-left: auto;
@@ -26,12 +25,10 @@ main {
 }
 
 @media (min-width: 1024px) {
-
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 }
-
 </style>
--- src/api/short.ts
+++ src/api/short.ts
@@ -1,22 +1,21 @@
-import data from './token.json'
+import data from "./token.json";
 
-const url = 'https://api-ssl.bitly.com/v4/shorten'
+const url = "https://api-ssl.bitly.com/v4/shorten";
 
 const shortenLink = async (long: string): Promise<any> => {
-    const {token, group_guid} = data;
-    return fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'group_guid': group_guid,
-            'domain': 'bit.ly',
-            'long_url': long,
-        })
-        
-    })
-}
+  const { token, group_guid } = data;
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      group_guid: group_guid,
+      domain: "bit.ly",
+      long_url: long,
+    }),
+  });
+};
 
 export default shortenLink;
